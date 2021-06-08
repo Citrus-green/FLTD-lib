447c7a4d6122e87317ef25c2109ed44175cb0ba9
Citrus <85241340+Citrus-green@users.noreply.github.com>
2021-06-08 11:02:52 +0900

    add some function

--- FLTD-lib/FltdNGS.cs
+++ FLTD-lib/FltdNGS.cs
@@ -8,17 +8,17 @@ using System.IO;
 namespace FLTD_lib
 {
 	unsafe public class FltdNGS
-    {
+	{
 
 		private stfltd data_array;
 		private StNIFL mStNIFL;
-		
-		public FltdNGS() { 
-		
+
+		public FltdNGS() {
+
 		}
 		public void loadFile(String path)
 		{
-			using (var fp = new BinaryIOHelper(new FileStream(path, FileMode.Open, FileAccess.Read),false))
+			using (var fp = new BinaryIOHelper(new FileStream(path, FileMode.Open, FileAccess.Read), false))
 			{
 				mStNIFL = new StNIFL(fp);
 				fp.SetSkip(mStNIFL.mNIFL.rel0_offset);
@@ -26,15 +26,28 @@ namespace FLTD_lib
 				data_array = new stfltd(fp);
 			}
 		}
-		private void readStFltd(BinaryReader fp) { 
-		
+		private void readStFltd(BinaryReader fp) {
+
 		}
 		public string[] GetAssignList()
-        {
+		{
 			string[] str = new string[data_array.count_addr0];
 			for (int i = 0; i < data_array.count_addr0; i++)
 				str[i] = data_array.data0[i].nameList[0];
 			return str;
 		}
+		public string[] GetRootMode(int index)
+        {
+			return data_array.data0[index].nameList;
+		}
+		public string[] GetConstraintList(int index) {
+			string[] nameList = new string[data_array.data0[index].count_addr1];
+			for(int i=0;i< data_array.data0[index].count_addr1; i++)
+            {
+				nameList[i] = data_array.data0[index].data3[i].name;
+			}
+			return nameList;
+		}
+
 	}
 }
